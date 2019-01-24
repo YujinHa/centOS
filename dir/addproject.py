33dbbc8c0c7c2263d5a182ea1078a218720f7846
hayujin <hamonica98@naver.com>
2019-01-24 11:50:39 +0900

    rm dir

--- dir/addproject.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import sys
-import os
-
-a = ['','/shot','/in','/out','/edit','/asset','/asset/char','/asset/shader','/config','/config/ocio','/config/thumbnail','/doc','/doc/concept']
-for i in a:
-	os.makedirs("/project/%s%s" %(sys.argv[1],i))
