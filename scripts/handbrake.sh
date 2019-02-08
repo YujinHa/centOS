2d13a9f840e496f67e04a3600b7a149aa6790ecb
YujinHa <hamonica98@naver.com>
2019-02-08 15:32:15 +0900

    오타 수정함

--- scripts/handbrake.sh
+++ scripts/handbrake.sh
@@ -4,7 +4,7 @@ if [[ $EUID -ne 0 ]]; then #EUID(사용자계정번호)가 0 -> 루트계정/999
 	exit 1
 fi
 yum update
-yum groupinstall Development Tools Additional Development
+yum groupinstall "Development Tools" "Additional Development"
 yum install fribidi-devel git jansson-devel libogg-devel libsamplerate-devel libtheora-devel libvorbis-devel opus-devel
 yum install libass-devel yasm
 yum repo-pkgs zmrepo remove
@@ -18,7 +18,8 @@ cd /home/$USER/app
 git clone https://github.com/HandBrake/HandBrake.git
 cd HandBrake
 git tag --list | grep ^1\.1\.
-git checkout refs/tags/
+git checkout refs/tags/$(git tag -l | grep -E '^1\.1\.[0-9]+$' | tail -n 1)
 ./configure --launch-jobs=16 --launch
 make --directory=build install
 rm -rf build
+
