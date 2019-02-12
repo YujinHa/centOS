1e5ee602a40fa15c904c4cb1a2d26e86653801ff
YujinHa <hamonica98@naver.com>
2019-02-12 12:14:29 +0900

    ocio, openexr 추가함

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -8,3 +8,12 @@ pip install --user pymediainfo
 pip install --user PySide2
 pip install --user numpy
 pip install --user Cython
+pip install --user openpyxl
+pip install --user xlswriter
+pip install --user xlrd
+pip install --user xlwt
+pip install --user xlutils
+pip install --user pillow
+pip install --user timecode
+pip install --user edl
+pip install --user opentimelineio
