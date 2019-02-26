596a40ab3d4ce80f2730e1bda775ae7979753e0e
YujinHa <hamonica98@naver.com>
2019-02-26 15:46:34 +0900

    rmdeep 추가함

--- scripts/ocio_config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ ! -d "/home/$USER/app" ]; then
-	mkdir /home/$USER/app
-fi
-
-cd /home/$USER/app
-git clone https://github.com/imageworks/OpenColorIO-Configs
--- scripts/rmhotcorner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [[ $EUID -ne 0 ]]; then
-	echo "This script must be run as Root" 1>&2
-	exit
-fi
-yum -y install gnome-tweak-tool
-yum -y install gnome-shell-extension-no-hot-corner
-# $ gnome-tweaks
